21dcad8c8448008bbf146ba866f18de40c2a9d63
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-07-29 12:12:09 +0000

    Fri, Jul 29, 2022, 12:12 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,12 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void pyramid(int h)
+{
+    if(h>0 && h<=8)
+    {
+        for(int i=)
+    }
+}
+
 int main(void)
 {
-    int height = get_int("Height: ");
-    if(height)
-    {
-
-    }
 
 }
