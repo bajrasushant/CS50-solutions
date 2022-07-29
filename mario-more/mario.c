1fcbe9394363d5f4e530dc4f22694f96d601a0c6
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-07-29 13:31:41 +0000

    Fri, Jul 29, 2022, 1:31 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,7 +4,12 @@
 int main(void)
 {
     int height = get_int("Height: ");
-    for(int i = 0; i<height; i++){
-        printf("#\n");
+    for(int i = 0; i<height; i++)
+    {
+        for(int j = height; j<0; j--)
+        {
+            printf("#");
     }
+    printf("\n");
+}
 }
