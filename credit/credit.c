73692d8c5680684b481b38ca39c1cbaba2e3adde
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-07-31 11:54:34 +0000

    Sun, Jul 31, 2022, 11:54 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
+#include<string.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long int card_num = 345678923;
+    int temp = 0;
+    for(int i = 0; i<=strlen(str)card_num; i+2)
+    {
+        temp = temp + i;
+    }
+    printf("%d", temp);
+    return 0;
 }
