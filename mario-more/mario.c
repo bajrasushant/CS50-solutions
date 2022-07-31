a58e568c6c6e5482ec36dd3cf1286619f6d309ca
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-07-31 09:05:43 +0000

    Sun, Jul 31, 2022, 9:05 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,15 +1,43 @@
-#include <cs50.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<cs50.h>
+void pyramid(int h)
+{
+    for(int i = 1; i <= h; i++)
+    {
+        for(int j = h; j > 0; j--)
+        {
+            if(i>=j)
+            {
+                printf("#");
+            }
+            else
+            {
+            printf(" ");
+            }
+        }
+        printf("\t");
+        for(int j = 1; j <= h; j++)
+        {
+            if(i>=j)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
 
 int main(void)
 {
-    int height = get_int("Height: ");
-    for(int i = 0; i<height; i++)
+    int height;
+    do
     {
-        for(int j = height; j<0; j--)
-        {
-            printf("#");
-    }
-    printf("\n");
-}
+        height = get_int("Height: ");
+    }while((height <= 0) || (height > 8));
+    pyramid(height);
+    return 0;
 }
