7310a912ebcb6f1645c2b4230396f48889e0134f
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-08-01 09:31:47 +0000

    Mon, Aug 1, 2022, 9:31 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-#include<cs50.>
+#include<cs50.h>
 
 int luhn(long int c_num)
 {
@@ -29,8 +29,8 @@ int luhn(long int c_num)
             sum_second_last = sum_second_last + temp_temp_second_last;
         }
     }
-    printf("Sum last is: %d\n", sum_last);
-    printf("Sum second last is: %d\n", sum_second_last);
+    // printf("Sum last is: %d\n", sum_last);
+    // printf("Sum second last is: %d\n", sum_second_last);
     return (sum_last+sum_second_last);
 }
 
@@ -40,22 +40,23 @@ void card_company(int card)
     int card_length = log10(card) + 1; //finds card length
     int start_two; //for finding the first two digits
     start_two = card/(pow(10, card_length - 2));
+    int start_one = card/pow(10, card_length -1);
     if(start_two == 34 || 37)
     {
         printf("AMEX\n");
     }
-    // else if ()
-    // {
-    //     printf("MASTERCARD\n");
-    // }
-    // else if
-    // {
-    //     printf("VISA\n");
-    // }
-    // else
-    // {
-    //     printf("A valid card. But don't know the origin.\n");
-    // }
+    else if (start_two == 51 || 52 || 53 || 54 || 55)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if(start_one == 4)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("A valid card. But don't know the origin.\n");
+    }
 }
 
 
