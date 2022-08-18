7e53c474e0c1bb0af3614f71553107a499218432
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-08-18 01:52:39 +0000

    Thu, Aug 18, 2022, 1:52 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -119,9 +119,11 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j <= candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
-            if (ranks[i] > ranks[j])
+           preferences[ranks[i]][ranks[j]]++;
+        }
+    }
     return;
 }
 
