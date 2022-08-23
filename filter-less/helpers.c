62d3c8b0c5a207b5ef2de489a746d20091f74d85
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-08-23 09:22:44 +0000

    Tue, Aug 23, 2022, 9:22 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,18 +8,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtBlue > image[i][j].rgbtGreen && image[i][j].rgbtBlue > image[i][j].rgbtRed)
-            {
-                grey_colour = image[i][j].rgbtBlue;
-            }
-            else if (image[i][j].rgbtBlue < image[i][j].rgbtGreen && image[i][j].rgbtGreen > image[i][j].rgbtRed)
-            {
-                grey_colour = image[i][j].rgbtGreen;
-            }
-            else
-            {
-                grey_colour = image[i][j].rgbtRed;
-            }
+            // if (image[i][j].rgbtBlue > image[i][j].rgbtGreen && image[i][j].rgbtBlue > image[i][j].rgbtRed)
+            // {
+            //     grey_colour = image[i][j].rgbtBlue;
+            // }
+            // else if (image[i][j].rgbtBlue < image[i][j].rgbtGreen && image[i][j].rgbtGreen > image[i][j].rgbtRed)
+            // {
+            //     grey_colour = image[i][j].rgbtGreen;
+            // }
+            // else
+            // {
+            //     grey_colour = image[i][j].rgbtRed;
+            // }
             int grey_shade;
             grey_shade = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
             image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = grey_shade;
