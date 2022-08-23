7d7b61a73bca2a987688884ed33c5c171c0a95e4
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-08-23 09:19:18 +0000

    Tue, Aug 23, 2022, 9:19 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,23 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue[
+            if (image[i][j].rgbtBlue > image[i][j].rgbtGreen && image[i][j].rgbtBlue > image[i][j].rgbtRed)
+            {
+                grey_colour = image[i][j].rgbtBlue;
+            }
+            else if (image[i][j].rgbtBlue < image[i][j].rgbtGreen && image[i][j].rgbtGreen > image[i][j].rgbtRed)
+            {
+                grey_colour = image[i][j].rgbtGreen;
+            }
+            else
+            {
+                grey_colour = image[i][j].rgbtRed;
+            }
+            int grey_shade;
+            grey_shade = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = grey_shade;
+        }
+    }
     return;
 }
 
