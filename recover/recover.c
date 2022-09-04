8bb6e35eb77757ce2b81adede3097deff7a4332a
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-09-04 00:43:45 +0000

    Sun, Sep 4, 2022, 12:43 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -19,22 +22,52 @@ int main(int argc, char *argv[])
     //fread(data,size,number,inptr) data-> to store data you're reading
     //size->size of each element to read number->number of elements to read inptr->File to read from
 
-    char buffer[512];
-    int count = 0;
+    BYTE buffer[512];
+    int count = -1;
     char filename[8];
     FILE *recovered_img;
 
     while(fread(buffer, 512, 1, file))
     {
-        if(buffer[0] = 0xff &&
-            buffer[1] = 0xd8 &&
-            buffer[2] = 0xff &&
+        if(buffer[0] == 0xff &&
+            buffer[1] == 0xd8 &&
+            buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
         {
+            //if it is not the first image
+            if (count > -1)
+            {
+                fclose(recovered_img);
+            }
+            //else it can start reading the images
+            count++;
             sprintf(filename,  "%03i.jpg", count);
             recovered_img = fopen(filename, "w");
-            count++;
+            if (recovered_img == NULL)
+            {
+                fclose(file);
+                printf("Couldn't create recovery image file.\n");
+                return 1;
+            }
+        }
+        if (count > -1)
+        {
+            fwrite(buffer, 512, 1, recovered_img);
+        }
+    }
 
+        fclose(file);
 
-
+        if (!recovered_img)
+        {
+            printf("Can't recover the files try a different file.");
+        }
+        else
+        {
+            fclose(recovered_img);
+        }
+        return 0;
 }
+
+
+
