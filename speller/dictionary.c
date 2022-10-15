a1b352f0fb7f853552707b15de2130c982002dbc
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-15 15:07:31 +0000

    Sat, Oct 15, 2022, 3:07 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -82,11 +82,11 @@ bool load(const char *dictionary)
 
         int hash_num = hash(words);
 
-        //if (table[hash_num] != NULL)
-
+        if (table[hash_num] != NULL)
+        {
             new_node->next = table[hash_num];
-
-
+        }
+        
         table[hash_num] = new_node;
 
         words_dict++;
