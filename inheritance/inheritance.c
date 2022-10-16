df11beabc935559201ce05a2fde9f44e1d6bebf3
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-16 10:05:26 +0000

    Sun, Oct 16, 2022, 10:05 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,9 +50,11 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        child_1->parents[0] = parent0;
+        child_1->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        
     }
 
     // If there are no generations left to create
