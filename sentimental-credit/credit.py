9bc1c12176087f5ac55ae994e40b02f10cbed5f9
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-25 03:30:50 +0000

    Tue, Oct 25, 2022, 3:30 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,27 +1,36 @@
+# from cs50 import get_int
+# import re
+
+# number = get_int("Number: ")
+# sum = 0
+
+# for i in range(len(str(number))-1, -1, -2):
+#     n = 2 * int(str(number)[i-1])
+#     if (n > 10):
+#         while n != 0:
+#             rem = n % 10
+#             sum = sum + rem
+#             n = n // 10
+#     else:
+#         sum = sum + n
+
+
+# if (sum % 10 == 0):
+#     str_number = str(number);
+#     if (str_number.startswith("37") or str_number.startswith("34")):
+#         print("AMEX")
+#     elif (re.search("^[5][1-5]", str_number)):
+#         print("MASTERCARD")
+#     elif (re.search("^4"), str_number):
+#         print("VISA")
+# else:
+#     print("INVALID")
+
 from cs50 import get_int
-import re
+from re import search
 
 number = get_int("Number: ")
-sum = 0
+str_number = str(number)
 
-for i in range(len(str(number))-1, -1, -2):
-    n = 2 * int(str(number)[i-1])
-    if (n > 10):
-        while n != 0:
-            rem = n % 10
-            sum = sum + rem
-            n = n // 10
-    else:
-        sum = sum + n
-
-
-if (sum % 10 == 0):
-    str_number = str(number);
-    if (str_number.startswith("37") or str_number.startswith("34")):
-        print("AMEX")
-    elif (re.search("^[5][1-5]", str_number)):
-        print("MASTERCARD")
-    elif (re.search("^4"), str_number):
-        print("VISA")
-else:
-    print("INVALID")
+for i in range(len(str_number)-1, -1, -2):
+    
