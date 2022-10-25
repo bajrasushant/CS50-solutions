2eff9980e6b21107dd39d7da74599b99441fa580
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-25 04:03:34 +0000

    Tue, Oct 25, 2022, 4:03 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,6 +8,7 @@ sum = 0
 
 #second last characters step - 2
 for i in range(len(str_number)-2, -1, -2):
+    #len - 2 as if length is 15 then we need the 13th element of the array(second last)
     n = 2 * int(str_number[i])
     if (n > 10):
         while n != 0:
@@ -16,15 +17,17 @@ for i in range(len(str_number)-2, -1, -2):
             sum = sum + rem
     else:
         sum = sum + n
-    print(f"sum:{sum}")
 
+#remaining elements
 for i in range(len(str_number)-1, -1, -2):
     sum = sum + int(str_number[i])
 print(f"sum: {sum}")
 
+#checking
 if (sum % 10 == 0):
     if (re.search("^34|^37", str_number)):
         print("AMEX")
+    elif (re.search("^[5]))
     else:
         print("VALID BUT INVALID")
 else:
