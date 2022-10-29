a2e4c4fd779fcc3621d9bab2b4376a842718791c
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-29 04:17:59 +0000

    Sat, Oct 29, 2022, 4:17 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -27,8 +27,8 @@ def main():
         sq_reader = csv.reader(file_sequence)
     # TODO: Find longest match of each STR in DNA sequence
         longest_run = 0
-        for i in sq_reader:
-            
+        for  in sq_reader:
+
     # TODO: Check database for matching profiles
 
     return
