d1b7c00f514dee7f454baf99b0f43fc78751e24b
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-29 04:48:04 +0000

    Sat, Oct 29, 2022, 4:48 AM +00:00

--- dna/try.py
+++ dna/try.py
@@ -1,4 +1,34 @@
-names = ["sushant", "yoju"]
-str = sushant
+# Python3 program to implement
+# the above approach
 
-for i in 
+# Function to count the maximum
+# consecutive occurrence of the
+# string str2 in the string str1
+def maxRepeating(str1, str2):
+
+	# Stores the count of consecutive
+	# occurrences of str2 in str1
+	cntOcc = str1.count(str2)
+
+	# Concatenate str2 cntOcc times
+	Contstr = str2 * cntOcc
+
+
+	# Iterate over the string str1
+	# while Contstr is not present in str1
+	while(Contstr not in str1):
+
+		# Update cntOcc
+		cntOcc -= 1
+
+	# Update Contstr
+		Contstr = str2 * cntOcc
+
+	return cntOcc
+
+# Driver Code
+if __name__ =="__main__":
+str1 = "abababc"
+str2 = "ba"
+print(maxRepeating(str1, str2))
+
