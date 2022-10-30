e4b65deb842c19fbb446b1a64c47e7383a7fc3d7
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-30 03:15:59 +0000

    Sun, Oct 30, 2022, 3:15 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -20,11 +20,12 @@ def main():
     file = open(sys.argv[1], "r")
     reader = csv.DictReader(file)
     for row in reader:
-        #teamsdict[row["team"]] = int(row["rating"])
-        teamsdict["team"] = row["team"]
-        teamsdict["rating"] = int(row["rating"])
+        teamsdict[row["team"]] = int(row["rating"])
+        # teamsdict["team"] = row["team"]
+        # teamsdict["rating"] = int(row["rating"])
 
         teams.append(teamsdict)
+    print(teams)
 
     file.close()
 
