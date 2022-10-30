f4cc07f6b2fee56316da87ebe9d95f9be5e43b76
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-10-30 01:18:04 +0000

    Sun, Oct 30, 2022, 1:18 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    file = open(sys.argv[1], "r")
+    reader = csv.reader(file)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
