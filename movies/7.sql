6cea52580980d0446e69bd786167065f65b49f58
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 03:22:53 +0000

    Tue, Nov 8, 2022, 3:22 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,4 @@
-SELECT title, rating FROM movies, ratings WHERE movies.id = ratings.movie_id AND movies.year = 2010 ORDER BY ratings.rating DESC, movies.title ASC;
+SELECT title, rating FROM movies, ratings
+WHERE movies.id = ratings.movie_id
+AND movies.year = 2010
+ORDER BY ratings.rating DESC, movies.title ASC;
