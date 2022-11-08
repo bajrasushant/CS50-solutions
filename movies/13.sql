dfab1a8f8f5634b01f0aa46baec863b43d82f236
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 05:55:14 +0000

    Tue, Nov 8, 2022, 5:55 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,8 @@
-SELECT title FROM movies, people, stars
+SELECT DISTINCT(name) from people, movies, stars
+WHERE movies.title =
+(SELECT title FROM movies, people, stars
 WHERE people.name = "Kevin Bacon"
 AND stars.person_id = people.id
-AND movies.id = stars.movie_id;
+AND movies.id = stars.movie_id)
+AND stars.movie_id = movies.id
+AND people.id = stars.person_id;
