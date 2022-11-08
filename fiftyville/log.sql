159b44b93ce148daee531798c3288fee4dfa0179
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 10:41:17 +0000

    Tue, Nov 8, 2022, 10:41 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,7 +6,7 @@ SELECT * FROM crime_scene_reports WHERE street = "Humphrey Street" AND month = 7
 
 SELECT * FROM interviews WHERE month = 7 AND day = 28;
 --Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
---Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
+--Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money. (Eugene)
 --As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
 --I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
 
