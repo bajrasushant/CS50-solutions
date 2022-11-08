a4be94fd7f3cd95d8c75e72343946b3788726c81
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 05:40:08 +0000

    Tue, Nov 8, 2022, 5:40 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,16 @@
+-- SELECT title FROM movies
+-- WHERE id in (SELECT movie_id FROM stars
+-- WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"))
+-- INTERSECT SELECT title FROM movies
+-- WHERE id in (SELECT movie_id FROM stars
+-- WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
+
 SELECT title FROM movies, stars, people
-WHERE people
+WHERE people.name = "Johnny Depp"
+AND stars.person_id = people.id
+AND movies.id = stars.movie_id
+INTERSECT
+SELECT title FROM movies, stars, people
+WHERE people.name = "Helena Bonham Carter"
+AND stars.person_id = people.id
+AND movies.id = stars.movie_id;
