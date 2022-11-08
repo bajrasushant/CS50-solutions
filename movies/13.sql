e2a73903bcea88a3a2dfd0eefba74e2eadec58cd
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 08:39:55 +0000

    Tue, Nov 8, 2022, 8:39 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -7,8 +7,10 @@
 -- AND stars.movie_id = movies.id
 -- AND people.id = stars.person_id;
 SELECT DISTINCT(name) FROM people, movies
-WHERE people.id IN (SELECT id FROM movies) (SELECT title FROM people, movies, stars
+WHERE people.id IN (SELECT person_id FROM stars
+WHERE movie_id IN (SELECT movie_id FROM stars
+WHERE title = (SELECT title FROM people, movies, stars
 WHERE people.name = "Kevin Bacon"
 AND people.birth = 1958
 AND stars.person_id = people.id
-AND movies.id = stars.movie_id);
+AND movies.id = stars.movie_id)));
