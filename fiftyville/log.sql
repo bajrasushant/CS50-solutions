b97d80b34a8e8fc3b2faf8ab69898ebc682bf134
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 10:39:35 +0000

    Tue, Nov 8, 2022, 10:39 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,3 +11,6 @@ SELECT * FROM interviews WHERE month = 7 AND day = 28;
 --I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
 
 SELECT * FROM bakery WHERE month = 7 AND day = 28;
+--13FNH73, 5P2BI95, 94KL13X, 6P58WS2, 4328GD8, G412CB7, L93JTIZ, 322W7JE, 0NTHK55 5 vehicles out in 10 minutes
+
+
