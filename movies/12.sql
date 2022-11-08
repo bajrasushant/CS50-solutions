e78259013c8008757944ccf2b84cfd44752faa83
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 05:31:49 +0000

    Tue, Nov 8, 2022, 5:31 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id
 AND people.id = ("Johnny Depp", "Helena Bonham Carter")
+AND stars.person_id = people.id;
