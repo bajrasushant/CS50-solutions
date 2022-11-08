e339de779d8bedbc9630204c4d1aca581704e9b4
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 04:48:24 +0000

    Tue, Nov 8, 2022, 4:48 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,8 @@
 SELECT titles FROM movies
 WHERE id IN
 (SELECT movie_id FROM stars
-WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"));
+WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp")) AS a
+SELECT titles FROM A
+WHERE id IN
+(SELECT movie_id FROM stars
+WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
