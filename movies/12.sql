73d0296d57a4d5fe7bfaf31a2cdc645451ca3cc7
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 05:35:04 +0000

    Tue, Nov 8, 2022, 5:35 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,6 @@
 SELECT title FROM movies, stars, people
 AND people.id = ("Johnny Depp", "Helena Bonham Carter")
 AND stars.person_id = people.id
-WHERE movies.id = stars.movie_id;
+WHERE movies.id = stars.movie_id
+GROUP BY movies.title
+HAVING COUNT(movies.title) = 2;
