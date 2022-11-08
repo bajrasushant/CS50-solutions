fea620121ed2fca7840741060f7baa09afadc923
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 09:09:11 +0000

    Tue, Nov 8, 2022, 9:09 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- the theft took place on july 28,2021 on Humphrey Street
-SELECT * FROM crime_scene_reports -- to check for crimes and details time
+SELECT * FROM crime_scene_reports; -- to check for crimes and details time
 --theft at 10:15, 3 witness, bakery
-SELECT * FROM bakery_security_logs -- to check who came at 10:15
+SELECT * FROM bakery_security_logs; -- to check who came at 10:15
 --at 10:14 13FNH73 entered(license plate)
+SELECT * FROM PEOPLE WHERE license_plate = "13FNH73";--Sophia vehicle 342612721(passport)
+--(027) 555-1068 (phone number)
+SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
