31145fbf9599a6612ec848f5d560a47c8053f0a1
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 03:51:18 +0000

    Tue, Nov 8, 2022, 3:51 AM +00:00

---
+++ movies/11.sql
@@ -0,0 +1,6 @@
+SELECT title FROM movies, ratings, people, stars
+WHERE people.name = "Chadwick Boseman"
+AND stars.person_id = people.id
+AND movies.id = stars.movie_id
+ORDER BY ratings.rating DESC
+LIMIT 5;
