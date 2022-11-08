bc54f8d46e84f0c3b0cc181c89d7cc005eb34d54
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 10:40:26 +0000

    Tue, Nov 8, 2022, 10:40 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,7 +10,7 @@ SELECT * FROM interviews WHERE month = 7 AND day = 28;
 --As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
 --I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
 
-SELECT * FROM bakery WHERE month = 7 AND day = 28;
+SELECT * FROM bakery_security_logs WHERE month = 7 AND day = 28;
 --13FNH73, 5P2BI95, 94KL13X, 6P58WS2, 4328GD8, G412CB7, L93JTIZ, 322W7JE, 0NTHK55 5 vehicles out in 10 minutes
 
 
