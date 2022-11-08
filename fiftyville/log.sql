c22e2b6b718750751a03309e49f4819fa60eceae
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 09:03:29 +0000

    Tue, Nov 8, 2022, 9:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- the theft took place on july 28,2021 on Humphrey Street
+SELECT * FROM crime_scene_reports -- to check for crimes and details time
+--theft at 10:15, 3 witness, bakery
+SELECT 
