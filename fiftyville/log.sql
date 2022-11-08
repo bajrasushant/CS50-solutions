6bfdc2dd440d8382d5e3eabcf243eb778708614b
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 09:05:41 +0000

    Tue, Nov 8, 2022, 9:05 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,4 +2,5 @@
 -- the theft took place on july 28,2021 on Humphrey Street
 SELECT * FROM crime_scene_reports -- to check for crimes and details time
 --theft at 10:15, 3 witness, bakery
-SELECT 
+SELECT * FROM bakery_security_logs -- to check who came at 10:15
+--at 10:14 13FNH73 entered(license plate)
