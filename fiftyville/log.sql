349ad16e574eda8f63c60e6da3d1667373811025
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 09:12:36 +0000

    Tue, Nov 8, 2022, 9:12 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,3 +7,6 @@ SELECT * FROM bakery_security_logs; -- to check who came at 10:15
 SELECT * FROM PEOPLE WHERE license_plate = "13FNH73";--Sophia vehicle 342612721(passport)
 --(027) 555-1068 (phone number)
 SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+--check footage after 10 mins car
+--cashing atm on earlier morning
+--earliest flight out of fiftyville the next day
