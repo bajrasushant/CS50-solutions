2e4c5e7a788c1dfd88b870d7081308d36b499bb7
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 10:33:38 +0000

    Tue, Nov 8, 2022, 10:33 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,3 +5,8 @@ SELECT * FROM crime_scene_reports WHERE street = "Humphrey Street" AND month = 7
 -- each of their interview transcripts mentions the bakery.
 
 SELECT * FROM interviews WHERE month = 7 AND day = 28;
+--Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+--Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
+--As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
+--I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
+
