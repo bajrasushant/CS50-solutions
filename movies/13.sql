4a5bc34a489c90e2a1b6d6aabe454bdf15efbf30
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-08 05:52:26 +0000

    Tue, Nov 8, 2022, 5:52 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,4 @@
 SELECT title FROM movies, people, stars
 WHERE people.name = "Kevin Bacon"
 AND stars.person_id = people.id
-AND movies.id = stars.movie_id
-GROUP BY movies.title;
+AND movies.id = stars.movie_id;
