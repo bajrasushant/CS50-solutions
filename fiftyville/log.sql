9386bd7f0936c481104be5e462ced090333b77be
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-09 01:34:40 +0000

    Wed, Nov 9, 2022, 1:34 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -152,3 +152,39 @@ WHERE id IN
             (SELECT account_number FROM atm_transactions
                 WHERE month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw")));
                 --checking from common flight id
+
+
+SELECT flight_id FROM passengers WHERE passport_number IN
+(SELECT passport_number FROM people
+WHERE phone_number IN
+    (SELECT receiver FROM phone_calls
+    WHERE month = 7 AND day = 28 AND duration < 60)
+INTERSECT
+SELECT passport_number FROM passengers
+WHERE flight_id IN
+        (SELECT id FROM flights
+        WHERE origin_airport_id = 8 AND month = 7 AND day = 29))
+INTERSECT
+SELECT flight_id from passengers WHERE passport_number IN(
+SELECT passport_number FROM passengers
+        WHERE flight_id IN
+            (SELECT id FROM flights
+                WHERE origin_airport_id = 8 AND month = 7 AND day = 29)
+INTERSECT
+SELECT passport_number FROM people WHERE license_plate IN
+    (SELECT license_plate FROM bakery_security_logs
+        WHERE month = 7 AND day = 28 AND hour = 10)
+INTERSECT
+SELECT passport_number FROM people
+WHERE phone_number IN
+    (SELECT caller FROM phone_calls
+    WHERE month = 7 AND day = 28 AND duration < 60)
+INTERSECT
+SELECT passport_number FROM people
+WHERE id IN
+    (SELECT person_id FROM bank_accounts
+        WHERE account_number in
+            (SELECT account_number FROM atm_transactions
+                WHERE month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw")));
+                -- flight 36
+
