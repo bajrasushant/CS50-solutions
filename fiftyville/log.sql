ea92cf9e99988e40a7d45279a381b7b78c310326
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-09 01:03:16 +0000

    Wed, Nov 9, 2022, 1:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -84,3 +84,18 @@ SELECT id, destination_airport_id FROM flights WHERE origin_airport_id = 8 AND m
 
 SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE origin_airport_id = 8 AND month = 7 AND day = 29));
 INTERSECT
+SELECT name FROM people WHERE license_plate IN
+    (SELECT license_plate FROM bakery_security_logs
+    WHERE month = 7 AND day = 28 AND hour = 10)
+INTERSECT
+SELECT name FROM people
+WHERE phone_number IN
+    (SELECT caller FROM phone_calls
+    WHERE month = 7 AND day = 28 AND duration < 60)
+INTERSECT
+SELECT name FROM people
+WHERE id IN
+    (SELECT person_id FROM bank_accounts
+        WHERE account_number in
+            (SELECT account_number FROM atm_transactions
+                WHERE month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw"));
