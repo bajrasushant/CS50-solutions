227177fbfa215b1d059c2fad21d2e4a35da28eda
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2022-11-09 01:33:09 +0000

    Wed, Nov 9, 2022, 1:33 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -151,3 +151,4 @@ WHERE id IN
         WHERE account_number in
             (SELECT account_number FROM atm_transactions
                 WHERE month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw")));
+                --checking from common flight id
