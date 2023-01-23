69b50c294290dd9245e1fb7b39910f28c185f741
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-23 05:13:43 +0000

    Mon, Jan 23, 2023, 5:13 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -139,7 +139,7 @@ def register():
             return apology("invalid username(username already taken)", 403)
         else:
             db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", username = request.form.get("username"), hash = generate_password_hash(password))
-
+            flash("You have been registered")
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
