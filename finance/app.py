2a783544555d4eece07fc46d4561292bfd8082a2
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-24 14:49:34 +0000

    Tue, Jan 24, 2023, 8:34 PM +05:45

--- finance/app.py
+++ finance/app.py
@@ -232,5 +232,9 @@ def sell():
             return apology("Not sufficient shares")
         result = share_owned[stock_to_sell] - num_shares_to_sell
 
-        db.execute
+        current_time = datetime.now().strftime("%H:%M:%S")
+        stock_current = lookup(stock_to_sell)
+        stock_price = stock_current["price"]
+        db.execute("DELETE FROM orders WHERE symbol=?", stock_to_sell)
+        db.execute("INSERT INTO orders(user_id, symbol, shares, price, time))
 
