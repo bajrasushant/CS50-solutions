abd2df63129b8039944f30b776ee72ada5c8678f
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-27 13:51:14 +0000

    Fri, Jan 27, 2023, 7:36 PM +05:45

--- volume/volume.c
+++ volume/volume.c
@@ -1,8 +1,9 @@
 // Modifies the volume of an audio file
-
+#include <cs50.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
@@ -34,8 +35,18 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    /* There is only one header, so parse it just once */
+    fread(&header, sizeof(header), 1, input);
+    fwrite(&header, sizeof(header), 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(buffer), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(buffer), 1, output);
+    }
 
     // Close files
     fclose(input);
