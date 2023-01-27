81f1dca613907c82af1c6d04c4e62d231601b7b5
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-27 04:03:33 +0000

    Fri, Jan 27, 2023, 9:48 AM +05:45

--- project/app.py
+++ project/app.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, flash, redirect, render_template, requeset, session
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session  import Session
 from cs50 import SQL
 from helpers import apology, login_required, password_check
@@ -16,10 +16,10 @@ try:
     db = SQL("sqlite:///final.db")
 except:
     db = sqlite3.connect('final')
-    db = conn.cursor()
+    # db = conn.cursor()
     db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL)")
-    db.execute("CREATE UNIQUE INDEC IF NOT EXISTS username ON users(username)")
-    conn.commit()
+    db.execute("CREATE UNIQUE INDEX IF NOT EXISTS username ON users(username)")
+    # conn.commit()
 
 @app.route("/")
 @login_required
