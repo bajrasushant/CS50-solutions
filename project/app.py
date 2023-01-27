c129490210cf0c1a1b223f90c7238485386721b6
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-27 03:43:43 +0000

    Fri, Jan 27, 2023, 9:28 AM +05:45

---
+++ project/app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, requeset, session
+from flask_session  import Session
+from cs50 import SQL
+from helpers import apology, login_required
+
+app = Flask(__name__)
+
+app.config["SESSSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///final.db")
+
+db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER, ))
