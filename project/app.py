95001c865b5871116e9ca882bbb0392ff2665a3b
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-01-27 03:50:42 +0000

    Fri, Jan 27, 2023, 9:35 AM +05:45

--- project/app.py
+++ project/app.py
@@ -1,9 +1,9 @@
 import os
+import sqlite3
 
 from flask import Flask, flash, redirect, render_template, requeset, session
 from flask_session  import Session
 from cs50 import SQL
-from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required
 
 app = Flask(__name__)
@@ -12,8 +12,15 @@ app.config["SESSSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///final.db")
+try:
+    db = SQL("sqlite:///final.db")
+except:
+    db = sqlite3.connect('final')
+    db = conn.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL)")
+    db.execute("CREATE UNIQUE INDEC IF NOT EXISTS username ON users(username)")
+    conn.commit()
 
-db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL)")
-db.execute("CREATE UNIQUE INDEC IF NOT EXISTS username ON users(username)")
+
+
 
