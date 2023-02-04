f8d55cfcb0852aeeb25193b5bedc9b8a0e2e2441
bajrasushant <84232901+bajrasushant@users.noreply.github.com>
2023-02-04 08:16:04 +0000

    Sat, Feb 4, 2023, 2:01 PM +05:45

--- project/app.py
+++ project/app.py
@@ -47,7 +47,7 @@ def todo():
 def update(todo_id):
     user_id = session["user_id"]
     todo = db.execute("SELECT * FROM todos WHERE id=? AND user_id=?", todo_id, user_id)
-    return redirect("/")
+    return render_template("update.html")
 
 @app.route("/done/<int:todo_id>")
 def done(todo_id):
